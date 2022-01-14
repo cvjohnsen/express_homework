f9433dff507f7a19eb4d67267cbc3312d7a1fc19
Cynthia Johnsen <vaj.cynthia@gmail.com>
2022-01-13 21:31:31 -0600

    3rd commit

--- routes/tipRoute.js
+++ routes/tipRoute.js
@@ -2,12 +2,13 @@ const express = require('express')
 const tipRoute = express.Router()
 
 tipRoute.get('/', (req, res) =>{
-res.status(200).json({message: "Hello, Stranger"})
+res.status(200).json({message: "Get those tips!"})
 })
 
-tipRoute.get('/:tip', (req, res) =>{
-let tip = (req.params.tip)
-res.status(200).json({message: "Hello, Stranger"})
+tipRoute.get('/:tip/:total/:tipPercentage', (req, res) =>{
+let tip = Number(req.params.tip)
+res.status(200).json({tips: 'tip % tipPercentage'})
 })
 
 module.exports = tipRoute
+
--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const bodyParser = require('body-parser')
 const port = 8888
 const app = express ()
 const greetingsRoute = require("./routes/greetingsRoute")
+const tipRoute = require('./routes/tipRoute')
 
 
 app.use(bodyParser.json())
-app.use ('/greet', greetingsRoute)
+app.use('/greet', greetingsRoute)
+app.use('/tip', tipRoute)
 
 app.get('/', (req, res) =>{
 res.status(200).json({message: "API UP!"})
